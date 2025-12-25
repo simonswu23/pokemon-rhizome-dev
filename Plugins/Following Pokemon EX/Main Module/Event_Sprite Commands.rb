46351b597a5af393886943388d1203159ff02006
Simon Wu <79566257+simonswu23@users.noreply.github.com>
2025-12-24 17:20:26 -0800

    Followers Added

---
+++ Plugins/Following Pokemon EX/Main Module/Event_Sprite Commands.rb
@@ -0,0 +1,169 @@
+module FollowingPkmn
+  #-----------------------------------------------------------------------------
+  # Script Command for getting the Following Pokemon event and corresponding
+  # Follower Data
+  #-----------------------------------------------------------------------------
+  def self.get
+    return nil if !FollowingPkmn.can_check?
+    $game_temp.followers.each_follower do |event, follower|
+      next if !follower.following_pkmn?
+      return [event, follower]
+    end
+    return nil
+  end
+  #-----------------------------------------------------------------------------
+  # Script Command for getting the Following Pokemon event
+  #-----------------------------------------------------------------------------
+  def self.get_event
+    return nil if !FollowingPkmn.can_check?
+    ret = FollowingPkmn.get
+    return ret.is_a?(Array) ? ret[0] : nil
+  end
+  #-----------------------------------------------------------------------------
+  # Script Command for getting the Following Pokemon FollowerData
+  #-----------------------------------------------------------------------------
+  def self.get_data
+    return nil if !FollowingPkmn.can_check?
+    ret = FollowingPkmn.get
+    return ret.is_a?(Array) ? ret[1] : nil
+  end
+  #-----------------------------------------------------------------------------
+  # Script Command for getting the Pokemon Object of the Following Pokemon
+  #-----------------------------------------------------------------------------
+  def self.get_pokemon
+    return nil if !FollowingPkmn.can_check?
+    return $player.first_able_pokemon
+  end
+  #-----------------------------------------------------------------------------
+  # Script Command for checking whether the current follower is airborne
+  #-----------------------------------------------------------------------------
+  def self.airborne_follower?
+    return false if !FollowingPkmn.can_check?
+    pkmn = FollowingPkmn.get_pokemon
+    return false if !pkmn
+    return true if pkmn.hasType?(:FLYING)
+    return true if pkmn.hasAbility?(:LEVITATE)
+    return true if FollowingPkmn::LEVITATING_FOLLOWERS.any? { |s| s == pkmn.species || s.to_s == "#{pkmn.species}_#{pkmn.form}" }
+    return false
+  end
+  #-----------------------------------------------------------------------------
+  # Script Command for checking if a swimming sprite exists for the Pokemon
+  #-----------------------------------------------------------------------------
+  def self.has_swimming_sprite?
+    return false if !FollowingPkmn.can_check?
+    pkmn = FollowingPkmn.get_pokemon
+    return false if !pkmn
+    
+    # Check if swimming sprite exists
+    shiny = pkmn.shiny?
+    shiny = pkmn.superVariant if (pkmn.respond_to?(:superVariant) && !pkmn.superVariant.nil? && pkmn.superShiny?)
+    
+    # Check for swimming sprite first
+    folder = shiny ? "Swimming Shiny" : "Swimming"
+    ret = GameData::Species.check_graphic_file("Graphics/Characters/", pkmn.species, pkmn.form,
+                                               pkmn.gender, shiny, pkmn.shadow, folder)
+    return true if !nil_or_empty?(ret)
+    
+    # Check for levitate sprite (for airborne Pokemon over water)
+    folder = shiny ? "Levitates Shiny" : "Levitates"
+    ret = GameData::Species.check_graphic_file("Graphics/Characters/", pkmn.species, pkmn.form,
+                                               pkmn.gender, shiny, pkmn.shadow, folder)
+    return !nil_or_empty?(ret)
+  end
+  #-----------------------------------------------------------------------------
+  # Script Command for checking whether the current follower is waterborne
+  #-----------------------------------------------------------------------------
+  def self.waterborne_follower?
+    return false if !FollowingPkmn.can_check?
+    pkmn = FollowingPkmn.get_pokemon
+    return false if !pkmn
+    
+    # Always follow if Pokemon is water type
+    return true if pkmn.hasType?(:WATER)
+    
+    # Always follow if the Pokemon has a swimming or levitate sprite available
+    # This takes priority over the exceptions list
+    return true if FollowingPkmn.has_swimming_sprite?
+    
+    # Check exceptions list before checking airborne
+    return false if FollowingPkmn::SURFING_FOLLOWERS_EXCEPTIONS.any? do |s|
+      s == pkmn.species || s.to_s == "#{pkmn.species}_#{pkmn.form}"
+    end
+    
+    # Follow if the Pokemon flies or levitates (and not in exceptions)
+    return true if FollowingPkmn.airborne_follower?
+    
+    return false
+  end
+  #-----------------------------------------------------------------------------
+  # Script Command for checking whether the current follower should use swimming sprites
+  #-----------------------------------------------------------------------------
+  def self.should_use_swimming_sprites?
+    return false if !FollowingPkmn.can_check? || !FollowingPkmn.active?
+    # Use swimming sprites only when player is actually surfing AND the follower is on water
+    return false if !$PokemonGlobal.surfing || !FollowingPkmn.waterborne_follower?
+    
+    # Check if the follower is actually on a water tile
+    event = FollowingPkmn.get_event
+    return false if !event
+    
+    # Check the terrain tag of the follower's current position
+    terrain_tag = $map_factory.getTerrainTag(event.map.map_id, event.x, event.y)
+    return terrain_tag.can_surf
+  end
+  #-----------------------------------------------------------------------------
+  # Forcefully refresh Following Pokemon sprite with animation (if specified)
+  #-----------------------------------------------------------------------------
+  def self.refresh(anim = false)
+    return if !FollowingPkmn.can_check?
+    event = FollowingPkmn.get_event
+    FollowingPkmn.remove_sprite
+    event&.calculate_bush_depth
+    first_pkmn = FollowingPkmn.get_pokemon
+    return if !first_pkmn
+    FollowingPkmn.refresh_internal
+    ret = FollowingPkmn.active?
+    event = FollowingPkmn.get_event
+    if anim
+      anim_name = ret ? :ANIMATION_COME_OUT : :ANIMATION_COME_IN
+      anim_id   = nil
+      anim_id   = FollowingPkmn.const_get(anim_name) if FollowingPkmn.const_defined?(anim_name)
+      if event && anim_id
+        $scene.spriteset.addUserAnimation(anim_id, event.x, event.y, false, 1)
+      end
+    end
+    FollowingPkmn.change_sprite(first_pkmn) if ret
+    FollowingPkmn.move_route([(ret ? PBMoveRoute::STEP_ANIME_ON : PBMoveRoute::STEP_ANIME_OFF)]) if FollowingPkmn::ALWAYS_ANIMATE
+    event&.calculate_bush_depth
+    $PokemonGlobal.time_taken = 0 if !ret
+    return ret
+  end
+  #-----------------------------------------------------------------------------
+  # Forcefully refresh Following Pokemon sprite with animation (if specified)
+  #-----------------------------------------------------------------------------
+  def self.remove_sprite
+    FollowingPkmn.get_event&.character_name = ""
+    FollowingPkmn.get_data&.character_name  = ""
+    FollowingPkmn.get_event&.character_hue  = 0
+    FollowingPkmn.get_data&.character_hue   = 0
+  end
+  #-----------------------------------------------------------------------------
+  # Set the Following Pokemon sprite to a different Pokemon
+  #-----------------------------------------------------------------------------
+  def self.change_sprite(pkmn)
+    shiny = pkmn.shiny?
+    shiny = pkmn.superVariant if (pkmn.respond_to?(:superVariant) && !pkmn.superVariant.nil? && pkmn.superShiny?)
+    swimming = FollowingPkmn.should_use_swimming_sprites?
+    fname = GameData::Species.ow_sprite_filename(pkmn.species, pkmn.form,
+      pkmn.gender, shiny, pkmn.shadow, swimming)
+    fname.gsub!("Graphics/Characters/", "")
+    FollowingPkmn.get_event&.character_name = fname
+    FollowingPkmn.get_data&.character_name  = fname
+    if FollowingPkmn.get_event&.move_route_forcing
+      hue = pkmn.respond_to?(:superHue) && pkmn.superShiny? ? pkmn.superHue : 0
+      FollowingPkmn.get_event&.character_hue  = hue
+      FollowingPkmn.get_data&.character_hue   = hue
+    end
+  end
+  #-----------------------------------------------------------------------------
+end
---
+++ Plugins/Following Pokemon EX/Script Additions/Field Moves.rb
@@ -0,0 +1,232 @@
+#-------------------------------------------------------------------------------
+# Aliased Surf call to not shown Following Pokemon Field move animation
+# when surfing
+#-------------------------------------------------------------------------------
+alias __followingpkmn__pbSurf pbSurf unless defined?(__followingpkmn__pbSurf)
+def pbSurf(*args)
+  $game_temp.no_follower_field_move = true
+  old_surfing = $PokemonGlobal.current_surfing
+  pkmn = $player.get_pokemon_with_move(:SURF)
+  $PokemonGlobal.current_surfing = pkmn
+  ret = __followingpkmn__pbSurf(*args)
+  $PokemonGlobal.current_surfing = old_surfing if !ret || !pkmn
+  $game_temp.no_follower_field_move = false
+  return ret
+end
+
+#-------------------------------------------------------------------------------
+# Aliaseds surf starting method to refresh Following Pokemon when the player
+# jumps to surf
+#-------------------------------------------------------------------------------
+alias __followingpkmn__pbStartSurfing pbStartSurfing unless defined?(__followingpkmn__pbStartSurfing)
+def pbStartSurfing(*args)
+  old_toggled = $PokemonGlobal.follower_toggled
+  surf_anim_1 = FollowingPkmn.active?
+  $PokemonGlobal.surfing = true
+  FollowingPkmn.refresh_internal
+  surf_anim_2 = FollowingPkmn.active?
+  $PokemonGlobal.surfing = false
+  FollowingPkmn.toggle_off(true) if surf_anim_1 != surf_anim_2
+  ret = __followingpkmn__pbStartSurfing(*args)
+  FollowingPkmn.toggle(old_toggled, false)
+  
+  # Simply move the follower one step in the player's direction
+  if FollowingPkmn.active?
+    event = FollowingPkmn.get_event
+    if event
+      case $game_player.direction
+      when 2 then event.move_down    # Player facing down
+      when 4 then event.move_left    # Player facing left
+      when 6 then event.move_right   # Player facing right
+      when 8 then event.move_up      # Player facing up
+      end
+      # Wait for the movement to complete
+      while event.moving?
+        Graphics.update
+        Input.update
+        pbUpdateSceneMap
+      end
+    end
+  end
+  
+  # Fix surf animation immediately by recalculating bush depth
+  FollowingPkmn.get_event&.calculate_bush_depth
+  # Refresh sprite to use swimming sprites if available
+  FollowingPkmn.refresh(false) if FollowingPkmn.active?
+  return ret
+end
+
+#-------------------------------------------------------------------------------
+# Aliased surf ending method to queue a refresh after the player jumps to stop
+# surfing
+#-------------------------------------------------------------------------------
+alias __followingpkmn__pbEndSurf pbEndSurf unless defined?(__followingpkmn__pbEndSurf)
+def pbEndSurf(*args)
+  surf_anim_1 = FollowingPkmn.active?
+  ret = __followingpkmn__pbEndSurf(*args)
+  return false if !ret
+  
+  # Let Following Pokemon use its normal follow logic when player exits water
+  $PokemonGlobal.current_surfing = nil
+  FollowingPkmn.refresh_internal
+  surf_anim_2 = FollowingPkmn.active?
+  $PokemonGlobal.call_refresh = [true, (surf_anim_1 != surf_anim_2), 1]
+  
+  # Simply move the follower one step in the player's direction
+  if FollowingPkmn.active?
+    event = FollowingPkmn.get_event
+    if event
+      case $game_player.direction
+      when 2 then event.move_down    # Player facing down
+      when 4 then event.move_left    # Player facing left
+      when 6 then event.move_right   # Player facing right
+      when 8 then event.move_up      # Player facing up
+      end
+      # Wait for the movement to complete
+      while event.moving?
+        Graphics.update
+        Input.update
+        pbUpdateSceneMap
+      end
+    end
+  end
+  
+  # Fix surf animation immediately by recalculating bush depth
+  FollowingPkmn.get_event&.calculate_bush_depth
+  # Refresh sprite to use normal sprites after surfing
+  FollowingPkmn.refresh(false) if FollowingPkmn.active?
+  return ret
+end
+
+#-------------------------------------------------------------------------------
+# Aliased Diving method to not show new HM Animation when diving
+#-------------------------------------------------------------------------------
+alias __followingpkmn__pbDive pbDive unless defined?(__followingpkmn__pbDive)
+def pbDive(*args)
+  $game_temp.no_follower_field_move = true
+  old_diving = $PokemonGlobal.current_diving
+  pkmn = $player.get_pokemon_with_move(:DIVE)
+  $PokemonGlobal.current_diving = pkmn
+  ret = __followingpkmn__pbDive(*args)
+  $PokemonGlobal.current_diving = old_diving if !ret || !pkmn
+  $game_temp.no_follower_field_move = false
+  # Fix dive animation immediately by recalculating bush depth
+  FollowingPkmn.get_event&.calculate_bush_depth if ret
+  return ret
+end
+
+#-------------------------------------------------------------------------------
+# Aliased surfacing method to not show new HM Animation when surfacing
+#-------------------------------------------------------------------------------
+alias __followingpkmn__pbSurfacing pbSurfacing unless defined?(__followingpkmn__pbSurfacing)
+def pbSurfacing(*args)
+  $game_temp.no_follower_field_move = true
+  old_diving = $PokemonGlobal.current_diving
+  $PokemonGlobal.current_diving = nil
+  ret = __followingpkmn__pbSurfacing(*args)
+  $PokemonGlobal.current_diving = old_diving if !ret
+  $game_temp.no_follower_field_move = false
+  # Fix surfacing animation immediately by recalculating bush depth
+  FollowingPkmn.get_event&.calculate_bush_depth if ret
+  return ret
+end
+
+#-------------------------------------------------------------------------------
+# Aliased hidden move usage method to not show new HM animation for certain
+# moves
+#-------------------------------------------------------------------------------
+alias __followingpkmn__pbUseHiddenMove pbUseHiddenMove unless defined?(__followingpkmn__pbUseHiddenMove)
+def pbUseHiddenMove(pokemon, move)
+  $game_temp.no_follower_field_move = [:SURF, :DIVE, :FLY, :DIG, :TELEPORT, :WATERFALL, :STRENGTH].include?(move)
+  if move == :SURF
+    old_data = $PokemonGlobal.current_surfing
+    $PokemonGlobal.current_surfing = pokemon
+  elsif move == :DIVE
+    old_data = $PokemonGlobal.current_diving
+    $PokemonGlobal.current_diving = pokemon
+  end
+  ret = __followingpkmn__pbUseHiddenMove(pokemon, move)
+  if move == :SURF
+    $PokemonGlobal.current_surfing = old_data if !ret
+  elsif move == :DIVE
+    $PokemonGlobal.current_diving = old_data if !ret
+  end
+  $game_temp.no_follower_field_move = false
+  return ret
+end
+
+#-------------------------------------------------------------------------------
+# Aliased Headbutt method to properly load Headbutt event for new HM Animation
+#-------------------------------------------------------------------------------
+alias __followingpkmn__pbHeadbutt pbHeadbutt unless defined?(__followingpkmn__pbHeadbutt)
+def pbHeadbutt(*args)
+  args[0] = $game_player.pbFacingEvent(true) if args[0].nil?
+  return __followingpkmn__pbHeadbutt(*args)
+end
+
+#-------------------------------------------------------------------------------
+# Aliased Waterfall methd to not show new HM Animation when interacting with
+# waterfall
+#-------------------------------------------------------------------------------
+alias __followingpkmn__pbWaterfall pbWaterfall unless defined?(__followingpkmn__pbWaterfall)
+def pbWaterfall(*args)
+  $game_temp.no_follower_field_move = true
+  $player.get_pokemon_with_move(:WATERFALL)
+  ret = __followingpkmn__pbWaterfall(*args)
+  $game_temp.no_follower_field_move = false
+  return ret
+end
+
+#-------------------------------------------------------------------------------
+# Aliased waterfall ascending method to make sure Following Pokemon properly
+# ascends the Waterfall with the player
+#-------------------------------------------------------------------------------
+def pbAscendWaterfall
+  return if $game_player.direction != 8   # Can't ascend if not facing up
+  terrain = $game_player.pbFacingTerrainTag
+  return if !terrain.waterfall && !terrain.waterfall_crest
+  $stats.waterfall_count += 1
+  oldthrough   = $game_player.through
+  oldmovespeed = $game_player.move_speed
+  $game_player.through    = true
+  $game_player.move_speed = 2
+  loop do
+    $game_player.move_up
+    terrain = $game_player.pbTerrainTag
+    break if !terrain.waterfall && !terrain.waterfall_crest
+    while $game_player.moving?
+      Graphics.update
+      Input.update
+      pbUpdateSceneMap
+    end
+  end
+  $game_player.through    = oldthrough
+  $game_player.move_speed = oldmovespeed
+end
+
+#-------------------------------------------------------------------------------
+# Aliased waterfall descending method to make sure Following Pokemon properly
+# descends the Waterfall with the player
+#-------------------------------------------------------------------------------
+def pbDescendWaterfall
+  return if $game_player.direction != 2   # Can't descend if not facing down
+  terrain = $game_player.pbFacingTerrainTag
+  return if !terrain.waterfall && !terrain.waterfall_crest
+  $stats.waterfalls_descended += 1
+  oldthrough   = $game_player.through
+  oldmovespeed = $game_player.move_speed
+  $game_player.through    = true
+  $game_player.move_speed = 2
+  loop do
+    $game_player.move_down
+    terrain = $game_player.pbTerrainTag
+    break if !terrain.waterfall && !terrain.waterfall_crest
+    while $game_player.moving?
+      Graphics.update
+      Input.update
+      pbUpdateSceneMap
+    end
+  end
+  $game_player.through    = oldthrough
+  $game_player.move_speed = oldmovespeed
+end
